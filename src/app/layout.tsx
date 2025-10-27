e79d330ce0174439da5a668f6dea6945e622ba9a
micci184 <micci184@gmail.com>
2025-10-27 22:58:04 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,9 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className="scroll-smooth">
-      <body
-        className={`${inter.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
         <Header />
         {children}
       </body>
